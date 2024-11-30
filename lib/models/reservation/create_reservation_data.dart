dcd89097c3e34e7f71b964d4a176ddd000d54886
pedro <pedroramirezneira@gmail.com>
2024-11-30 18:28:42 -0300

    fix: xoaco (boe que era)

--- lib/models/reservation/create_reservation_data.dart
+++ lib/models/reservation/create_reservation_data.dart
@@ -1,17 +1,19 @@
+// ignore_for_file: non_constant_identifier_names
+
 class CreateReservationData {
-  final int timeslotId;
+  final int timeslot_id;
   final String date;
   final String modality;
 
   const CreateReservationData({
-    required this.timeslotId,
+    required this.timeslot_id,
     required this.date,
     required this.modality,
   });
 
   Map<String, dynamic> toJson() {
     return {
-      'timeslotId': timeslotId,
+      'timeslot_id': timeslot_id,
       'date': date,
       'modality': modality,
     };
